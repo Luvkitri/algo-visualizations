b5d6b953547528886524f997dfefe50f900badb0
Luvkitri <janjedrzejewski@gmail.com>
2021-04-19 01:46:14 +0200

    Fixes to navbar so that side panel will be under it

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import { AppBar, Toolbar, Typography, Tabs, Tab } from '@material-ui/core';
 
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 import EqualizerIcon from '@material-ui/icons/Equalizer';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       textAlign: 'center',
+      zIndex: theme.zIndex.drawer + 1,
     },
     bar: {
       display: 'inline-block',
@@ -36,7 +37,7 @@ const Navbar: React.FC<Props> = ({ selectedTab, handleTabChange }) => {
   const classes = useStyles();
 
   return (
-    <AppBar position='static' className={classes.root}>
+    <AppBar position='fixed' className={classes.root}>
       <Toolbar className={classes.bar}>
         <Typography variant='h4' className={classes.title}>
           Algorithm-Visualizations
