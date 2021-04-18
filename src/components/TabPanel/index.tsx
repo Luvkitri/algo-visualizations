73ad075d9aafa6ba1fd59da63bd74af74726b6af
Luvkitri <janjedrzejewski@gmail.com>
2021-04-19 01:47:23 +0200

    Updateded tabpanel logic

--- src/components/TabPanel/index.tsx
+++ src/components/TabPanel/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import SortCanvas from '../SortCanvas';
+import Canvas from '../Canvas';
+import {} from '../../utils/Canvas/Sort/index';
 
 type Props = {
-  value: number,
-  index: number,
+  value: number;
+  index: number;
 };
 
 const TabPanel: React.FC<Props> = ({ value, index }) => {
-  return (
-    <div>
-      {value === index && (
-        <SortCanvas />
-      )}
-    </div>
-  )
+  const selectTab = (): string => {
+    switch (value) {
+      case 0:
+        return 'Hello sort';
+      case 1:
+        return 'Pathfinding hello';
+      case 2:
+        return 'Hello search';
+      default:
+        return 'Error: index out of range';
+    }
+  };
+
+  return <div>{selectTab()}</div>;
 };
 
 export default TabPanel;
